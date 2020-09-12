380fc3c9c8bbfb65d7069c563d9ab99cc186eb4d
Patricia Clares <patricia.clares@dasa.com.br>
2020-09-12 19:26:14 -0300

    first commit

---
+++ LifeShare/Models/Empresa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LifeShare.Models
+{
+    public class Empresa
+    {
+
+        //prop -> tab tab
+        [HiddenInput]
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        [Display(Name = "Data de Entrada")] //texto do label
+        [DataType(DataType.Date)] // input type="date"
+        public DateTime DataEntrada{ get; set; }
+        public Status Status { get; set; }
+
+    }
+}
