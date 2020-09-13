23560d7d34260dab03998fe7f636611a1dea78a4
Patricia Clares <patricia.clares@dasa.com.br>
2020-09-13 19:15:47 -0300

    gambi

--- LifeShare/LifeShare/Models/Empresa.cs
+++ LifeShare/LifeShare/Models/Empresa.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LifeShare.Models
 {
+    [Table("Tb_Empresa")]
     public class Empresa
     {
 
-        //prop -> tab tab
-        [HiddenInput]
-        public int Codigo { get; set; }
+        [Column("Id"),HiddenInput]
+        public int EmpresaId { get; set; }
+        [MaxLength(80)]
         public string Nome { get; set; }
-        [Display(Name = "Data de Entrada")] //texto do label
-        [DataType(DataType.Date)] // input type="date"
+        [Column("Dt_Entrada"), DataType(DataType.Date), Display(Name = "Data de Entrada")]
         public DateTime DataEntrada{ get; set; }
         public Status Status { get; set; }
 
